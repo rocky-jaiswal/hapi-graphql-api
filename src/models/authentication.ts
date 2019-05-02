774aeacdf367e6460b334fabcf1c645682311a6a
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2019-05-02 17:40:55 +0200

    minor.

--- src/models/authentication.ts
+++ src/models/authentication.ts
@@ -1,8 +1,8 @@
 import * as JWT from 'jsonwebtoken';
 import * as Crypto from 'crypto';
 
-export const SECRET = Crypto.randomBytes(512).toString('base64');
-const DUMMY_ID = Crypto.randomBytes(256).toString('base64');
+export const SECRET = Crypto.randomBytes(512).toString('hex');
+const DUMMY_ID = Crypto.randomBytes(256).toString('hex');
 
 export const createToken = () => {
   return JWT.sign({ id: DUMMY_ID }, SECRET);
