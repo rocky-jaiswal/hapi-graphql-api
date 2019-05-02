175f0525a3babb4c70e99e871338122a5cfee4fc
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2019-05-02 20:23:31 +0200

    Add SQL setup.

---
+++ knexfile.js
@@ -0,0 +1,8 @@
+const dbConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: { filename: './db/app_dev.db' },
+  migrations: { directory: './db/migrations', tableName: 'knex_migrations' }
+};
+
+module.exports = dbConfig;
